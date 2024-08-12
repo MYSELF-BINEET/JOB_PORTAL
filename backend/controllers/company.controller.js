7844e937e95b10519904f286e557254d4a4a8600
Bineet Pradhan <bineetpradhan03@gmail.com>
2024-08-12 10:41:49 +0530

    construct company and recruiter page

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import {Company} from "../models/company.model.js"
+import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../utils/dataUri.js";
 
 
 
@@ -79,7 +81,18 @@ export const updateCompany =async(req,res)=>{
     try{
         const {name,description,website,location}=req.body;
         const file=req.file;   // cloudinary
-        const updateData={ name,description,website,location };
+        if(!file){
+            return res.status(400).json({
+                message:"Please upload a file",
+                success:false
+            })
+        }
+
+        const fileUri=getDataUri(file);
+        const cloudResponse=await cloudinary.uploader.upload(fileUri.content);
+        const logo=cloudResponse.secure_url;
+
+        const updateData={ name,description,website,location,logo };
         const company= await Company.findByIdAndUpdate(req.params.id,updateData,{new:true});
 
         if(!company){
